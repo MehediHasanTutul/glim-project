b8ba46aeff4cbccad1fe567cfcff7c8c709dc10a
MehediHasanTutul <88667932+MehediHasanTutul@users.noreply.github.com>
2022-06-01 09:08:08 +1000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,4 @@
 mkdir -p ~/.streamlit/
-
-echo "[theme]
-primaryColor='#59708C'
-backgroundColor='#ECFCF3'
-secondaryBackgroundColor='#ECFCF9'
-textColor='#051614'
-font='sans serif'
 [server]\n\
 port = $PORT\n\
 enableCORS = false\n\
